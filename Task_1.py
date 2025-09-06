c100f1c74222a46003526d631cc95167ab212595
Aarush-R-790 <aarush.rout.62131121@gmail.com>
2025-09-06 21:43:54 +0530

    Completed task 1

---
+++ Task_1.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+print("/n")
+print("Addition :", a + b)
+print("Subtraction :", a - b)
+print("Multiplication :", a * b)
+print("Divsion :", a / b)
+print("/nTHANK YOU")
